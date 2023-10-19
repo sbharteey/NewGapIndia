49fa24fef3e01b200a4e8cc7700b694454ff4bd2
sbharteey <sbharteey@gmail.com>
2023-10-20 04:51:10 +0530

    vision style

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,4 @@
+// src/pages/index.js
 import React, { useEffect, useState } from 'react';
 import Layout from '../layout';
 import styles from '../styles/index.module.css';
@@ -30,19 +31,20 @@ const Home = () => {
 
     fetchGoogleDocsContent();
   }, []);
+  const formattedContent = content.replace(/<p>/g, '<p class="my-paragraph">');
 
   return (
     <Layout pageTitle="Vision">
       <div className={`${styles.container} ${styles.main}`}>
         <Image src="/images/sb small.png" alt="Image Description" width={200} height={200} className={styles.image} />
         <h1 className={styles.h1}></h1>
-        <div className={styles['google-docs-content']} dangerouslySetInnerHTML={{ __html: content }} />
-        <p>{
-        }
-        </p>
+        <div
+          className={styles['google-docs-content']}
+          dangerouslySetInnerHTML={{ __html: formattedContent }}
+        />
       </div>
     </Layout>
-  )
+  );
 }
 
 export default Home;
