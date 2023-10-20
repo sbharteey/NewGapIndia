db77d1b8c89631e00f936acd95cc5e2e1a18d8d9
Shyam Bharteey <sbharteey@gmail.com>
2023-10-20 19:40:47 +0530

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,7 @@
 // src/pages/_app.js
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { ReactQueryDevtools } from 'react-query/devtools';
-import '../styles/globals.css'; // Import your global styles here
+import '../styles/globals.module.css'; // Import your global styles here
 import toast, { Toaster } from 'react-hot-toast';
 
 const queryClient = new QueryClient();
