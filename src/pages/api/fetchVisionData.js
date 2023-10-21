2e96107d09dcb9090749ae8c986e81118db74332
sbharteey <sbharteey@gmail.com>
2023-10-21 18:37:39 +0530

    removed extra code

--- src/pages/api/fetchVisionData.js
+++ src/pages/api/fetchVisionData.js
@@ -3,6 +3,7 @@ import { fetchVisionDataFromGoogleDocs } from './fetchVisionDataFromGoogleDocs';
 export default async function handler(req, res) {
   try {
     const data = await fetchVisionDataFromGoogleDocs();
+    console.log(data);
     res.status(200).json(data);
   } catch (error) {
     console.error('Error:', error);
